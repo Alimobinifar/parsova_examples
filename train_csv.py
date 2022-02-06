aeb060e938b1c3e254cce297153eef0e58445bcc
Alimobinifar <88401300+Alimobinifar@users.noreply.github.com>
2022-02-06 14:33:01 +0330

    Update train_csv.py

--- train_csv.py
+++ train_csv.py
@@ -2,13 +2,13 @@ import csv
 
 file_url='empty.csv'
 
+# Read Csv File 
+
 with open(file_url,'r') as f :
     header=next(f)
     print(header.split(','))
-    # for i in f :
-    #     x=i.split()
-    #     print(x)
 
+# Write on csv as append
 with open (file_url,'a') as f:
     writer=csv.writer(f)
     writer.writerow(['mohsen','hamidi',63])

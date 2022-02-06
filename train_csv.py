5abb4ab9cbdca9437024ded85d22920d3d2e34d3
Alimobinifar <88401300+Alimobinifar@users.noreply.github.com>
2022-02-06 14:01:24 +0330

    Add files via upload

---
+++ train_csv.py
@@ -0,0 +1,14 @@
+import csv
+
+file_url='empty.csv'
+
+with open(file_url,'r') as f :
+    header=next(f)
+    print(header.split(','))
+    # for i in f :
+    #     x=i.split()
+    #     print(x)
+
+with open (file_url,'a') as f:
+    writer=csv.writer(f)
+    writer.writerow(['mohsen','modhej',63])
